5df0c4d073cf7370adafe4b25d60f7c5703385ea
Documenter.jl <documenter@juliadocs.github.io>
2022-09-27 20:06:08 +0000

    build based on f4f3088

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Findr","category":"page"},{"location":"#Findr","page":"Home","title":"Findr","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Findr.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Findr]","category":"page"}]
+}
