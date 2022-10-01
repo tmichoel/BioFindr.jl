95f0c327c0741f94f5e9d4cf1790831fb41fe3f0
tmichoel <11647967+tmichoel@users.noreply.github.com>
2022-10-01 12:32:23 +0200

    update

--- src/supernormalization.jl
+++ src/supernormalization.jl
@@ -1,9 +1,9 @@
 """
     supernormalize(X[, c])
 
-Convert each row of matrix `X` of reals into standard normally distributed values using a rank-based inverse normal transformation. 
+Convert each row of matrix `X` of reals into standard normally distributed values using a rank-based inverse normal transformation. Then scale each row to have variance one.
 
-After the inverse normal transformation, each row has mean zero (within numerical tolerance) and identical variance (if we use ordinal ranking). Hence scale each row to have variance equal to the sample size (number of columns)
+Note that after the inverse normal transformation, each row has mean zero and identical variance (if we use ordinal ranking). Hence rescaling can be done once on the whole matrix.
 
 The formula and default value for the paramater `c` come from this paper: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2921808/
 """
@@ -15,7 +15,9 @@ function supernormalize(X, c=0.375)
         Y[i,:] = map(x -> quantile(nd,(x-c)/(n-2c+1)), ordinalrank(X[i,:]))
     end
     σ = std(Y[1,:];corrected=false)
-    #Y = Y/σ
+    println(σ)
+    Y = Y/σ
     return Y
+
 end
 
