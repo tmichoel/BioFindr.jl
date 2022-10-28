0eedd51f829c6480510d4e98115b1d83e72819f4
tmichoel <11647967+tmichoel@users.noreply.github.com>
2022-10-28 19:10:50 +0200

    update realLLR

--- src/realLLR.jl
+++ src/realLLR.jl
@@ -11,11 +11,11 @@ function realLLRcorr_row(Y,row)
 end
 
 """
-    realLLRcorr_row(ρ)
+    realLLRcorr_row_sumstat(ρ)
 
 Compute the log-likelihood ratios for Findr test 0 (**correlation test**) with precomputed correlation values `̢ρ`
 """
-realLLRcorr_row(ρ) = -0.5*log.(1 .- ρ.^2)
+realLLRcorr_row_sumstat(ρ) = -0.5*log.(1 .- ρ.^2)
 
 """
     realLLRlink_row(Y,E)
@@ -30,11 +30,11 @@ function realLLRlink_row(Y,E)
 end
 
 """
-    realLLRlink_row(σ)
+    realLLRlink_row_sumstat(σ)
 
 Compute the log-likelihood ratios for Findr test 2 (**linkage test**) with precomputed weighted average group variances `σ`.
 """
-realLLRlink_row(σ) = -0.5*log.(σ)
+realLLRlink_row_sumstat(σ) = -0.5*log.(σ)
 
 """
     realLLRmed_row(Y,E,row)
@@ -51,11 +51,11 @@ function realLLRmed_row(Y,E,row)
 end
 
 """
-    realLLRmed_row(ρ,σ1,σ2)
+    realLLRmed_row_sumstat(ρ,σ1,σ2)
 
 Compute the log-likelihood ratios for Findr test 3 (**mediation test**) with precomputed correlation values `ρ` and weighted average group variances `σ1` and covariances `σ2`
 """
-realLLRmed_row(ρ,σ1,σ2) = -0.5*log(σ1[row].*σ1 .- (ρ .+ σ2 .- 1).^2) .+ 0.5*log(σ1[row]) + 0.5*log(1 .- ρ.^2)
+realLLRmed_row_sumstat(ρ,σ1,σ2) = -0.5*log(σ1[row].*σ1 .- (ρ .+ σ2 .- 1).^2) .+ 0.5*log(σ1[row]) + 0.5*log(1 .- ρ.^2)
 
 """
     realLLRrelev_row(Y,E,row)
@@ -72,11 +72,11 @@ function realLLRrelev_row(Y,E,row)
 end
 
 """
-    realLLRrelev_row(ρ,σ1,σ2)
+    realLLRrelev_row_sumstat(ρ,σ1,σ2)
 
 Compute the log-likelihood ratios for Findr test 3 (**mediation test**) with precomputed correlation values `ρ` and weighted average group variances `σ1` and covariances `σ2`
 """
-realLLRrelev_row(ρ,σ1,σ2) = -0.5*log(σ1[row].*σ1 .- (ρ .+ σ2 .- 1).^2) .+ 0.5*log(σ1[row])
+realLLRrelev_row_sumstat(ρ,σ1,σ2) = -0.5*log(σ1[row].*σ1 .- (ρ .+ σ2 .- 1).^2) .+ 0.5*log(σ1[row])
 
 
 """
@@ -94,12 +94,11 @@ function realLLRpleio_row(Y,E,row)
 end
 
 """
-    realLLRpleio_row(ρ,σ1,σ2)
+    realLLRpleio_row_sumstat(ρ,σ1,σ2)
 
 Compute the log-likelihood ratios for Findr test 3 (**mediation test**) with precomputed correlation values `ρ` and weighted average group variances `σ1` and covariances `σ2`
 """
-realLLRpleio_row(ρ,σ1,σ2) = -0.5*log(σ1[row].*σ1 .- (ρ .+ σ2 .- 1).^2) .+ 0.5*log(σ1[row].*σ1)
-
+realLLRpleio_row_sumstat(ρ,σ1,σ2) = -0.5*log(σ1[row].*σ1 .- (ρ .+ σ2 .- 1).^2) .+ 0.5*log(σ1[row].*σ1)
 
 """
     llrstats_row(Y,E,row)
@@ -110,15 +109,22 @@ Compute the sufficient statistics to compute the log-likelihood ratios for Findr
 
 The sufficient statistics are:
 
-- the group sizes (number of elements) for each group (unique value) in `E`
-- the mean of each row of matrix `Y` for each of the groups (unique values) in `E`
-- the weighted average variance of each row of matrix `Y` over the groups (unique values) in `E`
-- the weighted average covariance between the given `row` of `Y` and all other rows of `Y` over the groups (unique values) of `E`
+- the covariance `ρ` between the given `row` of matrix `Y` and all other rows of `Y`
+- the weighted average variances `σ1` of each row of matrix `Y` over the groups (unique values) in `E`
+- the weighted average covariance `σ2` between the given `row` of `Y` and all other rows of `Y` over the groups (unique values) of `E`
 """
 function llrstats_row(Y,E,row)
-    ρ = cov_row(Y,row)
-    σ1 = groupvar(Y,E)
-    σ2 = groupcov(Y,E,row)
+    ρ = cov(Y[row,:],Y,dims=2,corrected=false)'
+    ρ[row] = 1.
+
+    gs, μ = groupmeans(Y,E)
+    
+    ns = length(E) # number of samples
+    w = pweights(gs/ns) # probability weights
+
+    σ1 = vec(1 .- sum(μ.^2,w,dims=2))
+    σ2 = vec(1 .- sum(μ.*μ[row,:]',w,dims=2))
+
     ρ, σ1, σ2
 end
 
@@ -128,9 +134,10 @@ end
 Compute the covariance between a given `row` of matrix `Y` and all other rows of `Y`.
 """
 function cov_row(Y,row)
-    return cov(Y[row,:],Y,dims=2,corrected=false)'
+    v = cov(Y[row,:],Y,dims=2,corrected=false)'
+    v[row] = 1.0
+    return v
 end
-
 
 """
     groupcov_row(Y,E,row)
@@ -139,9 +146,9 @@ Compute the weighted average covariance between a given `row` of matrix `Y` and 
 """
 function groupcov(Y,E,row)
     ns = length(E) # number of samples
-    w = pweights(groupsizes(E)/ns) # probability weights
-    μ = groupmeans(Y,E)
-    return 1 .- sum(μ.*μ[row,:]',w,dims=2)
+    gs, μ = groupmeans(Y,E)
+    w = pweights(gs/ns) # probability weights
+    1 .- sum(μ.*μ[row,:]',w,dims=2)
 end
 
 """
@@ -151,35 +158,23 @@ Compute the weighted average variance of each row of matrix `Y` over the groups 
 """
 function groupvar(Y,E)
     ns = length(E) # number of samples
-    w = pweights(groupsizes(E)/ns) # probability weights
-    μ = groupmeans(Y,E)
-    return 1 .- sum(μ.^2,w,dims=2)
+    gs, μ = groupmeans(Y,E)
+    w = pweights(gs/ns) # probability weights
+    1 .- sum(μ.^2,w,dims=2)
 end
 
 """
     groupmeans(Y,E)
 
-Compute the mean of each row of matrix `Y` for each of the groups (unique values) in categorical vector `E`.
+Compute the size and mean of each row of matrix `Y` for each of the groups (unique values) in categorical vector `E`.
 """
 function groupmeans(Y,E)
     uE = unique(E)
+    gs = zeros(Int,length(uE))
     μ = zeros(size(Y)[1],length(uE))
     for i = eachindex(uE)
+        gs[i] = sum(E.==uE[i])
         μ[:,i] = mean(Y[:,E.==uE[i]], dims=2)
     end
-    return μ
-end
-
-"""
-    groupsizes(E)
-
-Compute the group sizes (number of elements) for each group (unique value) in categorical vector `E`.
-"""
-function groupsizes(E)
-    uE = unique(E)
-    gs = zeros(Int,length(uE))
-    for i = eachindex(uE)
-        gs[i] = sum(E.==uE[i])
-    end
-    return gs
+    gs, μ
 end
