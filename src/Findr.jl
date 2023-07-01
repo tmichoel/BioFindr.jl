6894664bb8307bc3a2893df21c4e497d5478ce1a
tmichoel <11647967+tmichoel@users.noreply.github.com>
2023-07-01 10:18:20 +0200

    update bipartite coexpression analysis

--- src/Findr.jl
+++ src/Findr.jl
@@ -44,11 +44,11 @@ include("utils.jl")
 
 Compute posterior probabilities for nonzero pairwise correlations between columns of input matrix `X`. The probabilities are directed (asymmetric) in the sense that they are estimated from a column-specific background distribution.
 
-The optional parameter `cols` determines whether we consider all columns as source nodes (`cols=[]`, default), or only a subset of columns determines by the indices in the vector `cols`.
+The optional parameter `cols` (vector of integers) determines whether we consider all columns of `X` as source nodes (`cols=[]`, default), or only a subset of columns determined by the indices in the vector `cols`.
 
 The optional parameter `method` determines the LLR mixture distribution fitting method and can be either `moments` (default) for the method of moments, or `kde` for kernel-based density estimation.
 
-The optional parameter `combination` determines whether the output must be symmetrized. Possible values are `none` (default), `prod`, `mean`, or `anti`. 
+The optional parameter `combination` determines whether the output must be symmetrized. Possible values are `none` (default), `prod`, `mean`, or `anti`. If the optional parameter `cols` is non-empty, symmetrization makes no sense and an error will be thrown unless `combination="none"`.
 
 See also [`findr(::DataFrame)`](@ref), [`symprobs`](@ref), [`supernormalize`](@ref), [`pprob_col`](@ref).
 """
@@ -75,13 +75,15 @@ end
 
 Wrapper for `findr(Matrix(dX))` when the input `dX` is in the form of a DataFrame. The output is then also wrapped in a DataFrame with `Source`, `Target`, (Posterior) `Probability`, and `qvalue` columns.
 
+The optional parameter `colnames` (vector of strings) determines whether we consider all columns of `dX` as source nodes (`colnames=[]`, default), or only a subset of columns determined by the variable names in the vector `colnames`.
+
 The optional parameter `method` determines the LLR mixture distribution fitting method and can be either `moments` (default) for the method of moments, or `kde` for kernel-based density estimation.
 
 The optional parameter `FDR` can be used to return only a subset of interactions with a desired expected FDR value (q-value threshold) (default 1.0, no filtering).
 
 The optional parameter `sorted` determines if the output must be sorted by increasing q-value / decreasing posterior probability (`sorted=true`, the default) or by causal factor (column names of `dX`) (`sorted=false`)
 
-The optional parameter `combination` determines whether the output must be symmetrized. Possible values are `none` (default), `prod`, `mean`, or `anti`. 
+The optional parameter `combination` determines whether the output must be symmetrized. Possible values are `none` (default), `prod`, `mean`, or `anti`. If the optional parameter `colnames` is non-empty, symmetrization makes no sense and an error will be thrown unless `combination="none"`.
 
 See also [`findr(::Matrix)`](@ref), [`symprobs`](@ref), [`stackprobs`](@ref), [`globalfdr!`](@ref).
 """
@@ -91,6 +93,31 @@ function findr(dX::T; colnames=[], method="moments", FDR=1.0, sorted=true, combi
     PP = findr(Matrix(dX); cols = cols, method = method, combination = combination)
     dP = stackprobs(PP, colnames, names(dX))
     globalfdr!(dP, FDR = FDR, sorted = sorted)
+    return dP
+end
+
+"""
+    findr(X1::Matrix{T}, X2::Matrix{T}; method="moments") where T<:AbstractFloat
+
+Compute posterior probabilities for nonzero pairwise correlations between columns of input matrices `X1` and `X2`. The probabilities are directed (asymmetric) from the columns of `X2` to the columns of `X1` in the sense that they are estimated from a column-specific background distribution for each column of `X2`.
+
+The optional parameter `method` determines the LLR mixture distribution fitting method and can be either `moments` (default) for the method of moments, or `kde` for kernel-based density estimation.
+
+Only use this method if `X1` and `X2` are distinct (no overlapping columns). For `X2` consisting of a subset of columns with indices `idx`, use `findr(X1; cols=idx)` instead. 
+
+See also [`findr(::DataFrame)`](@ref), [`symprobs`](@ref), [`supernormalize`](@ref), [`pprob_col`](@ref).
+"""
+function findr(X1::Matrix{T}, X2::Array{T}; method="moments") where T<:AbstractFloat
+    # Inverse-normal transformation and standardization for each columns of X1 and X2
+    Y1 = supernormalize(X1)
+    Y2 = supernormalize(X2)
+    # Matrix to store posterior probabilities
+    PP = ones(size(X1,2), size(X2,2))
+    # Compute posterior probabilities for each column separately
+    Threads.@threads for col = axes(Y2,2)
+        PP[:,col] = pprob_col(Y1,Y2[:,col]; method = method)
+    end
+    return PP
 end
 
 
@@ -137,11 +164,14 @@ The optional parameter `FDR` can be used to return only a subset of interactions
 
 The optional parameter `sorted` determines if the output must be sorted by increasing q-value / decreasing posterior probability (`sorted=true`, the default) or by causal factor (column names of `dX`) (`sorted=false`)
 
+Note that depending on the type of `Matrix(dG)`, different matrix-based methods are called. If `Matrix(dG)` consists of Floats, posterior probabilities for nonzero pairwise correlations between the variables in `dG` and variables in `dX` are computed. If `Matrix(dG)` consists of integers, posterior probabilities for nonzero differential expression of variables in `dX` across groups defined by the variables in `dG` are computed
+
 See also [`findr(::Matrix,::Array)`], [`stackprobs`](@ref), [`globalfdr!`](@ref). 
 """
 function findr(dX::T, dG::T; method="moments", FDR=1.0, sorted=true) where T<:AbstractDataFrame
     dP = stackprobs(findr(Matrix(dX), Matrix(dG); method = method), names(dG), names(dX))
     globalfdr!(dP, FDR = FDR, sorted = sorted)
+    return dP
 end
 
 #####################################
@@ -222,7 +252,8 @@ function findr(dX::T, dG::T, dE::T; colG=1, colX=2, method="moments", combinatio
         # Call Findr on numeric data
         PP = findr(Matrix(dX), Matrix(dG), pairGX; method = method, combination = combination)
         dP = stackprobs(PP, names(dX)[pairGX[:,2]], names(dX)) 
-        return globalfdr!(dP, FDR = FDR, sorted = sorted)  
+        globalfdr!(dP, FDR = FDR, sorted = sorted)
+        return dP
     else
         error("Combination parameter must be one of \"IV\", \"mediation\", or \"orig\"")
     end
@@ -297,7 +328,8 @@ function findr(dX1::T, dX2::T, dG::T, dE::T; colG=1, colX=2, method="moments", c
         # Call Findr on numeric data
         PP = findr(Matrix(dX1), Matrix(dX2), Matrix(dG), pairGX; method = method, combination = combination)
         dP = stackprobs(PP, names(dX2)[pairGX[:,2]], names(dX1))
-        return globalfdr!(dP, FDR = FDR, sorted = sorted)
+        globalfdr!(dP, FDR = FDR, sorted = sorted)
+        return dP
     else
         error("Combination parameter must be one of \"IV\", \"mediation\", or \"orig\"")
     end
