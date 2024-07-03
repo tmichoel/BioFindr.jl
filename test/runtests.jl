fabe647af5230a469bb51018f58d5ed2a0998d8a
tmichoel <11647967+tmichoel@users.noreply.github.com>
2024-07-03 14:55:12 +0200

    include all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,25 +14,25 @@ using DataFrames
 @testset "BioFindr.jl" begin
     # Write your tests here.
    
-    # @testset "Supernormalization" begin
-    #     include("supernormalization_tests.jl");
-    # end
+    @testset "Supernormalization" begin
+        include("supernormalization_tests.jl");
+    end
 
-    # @testset "Real LLRs" begin
-    #     include("realLLR_tests.jl");
-    # end
+    @testset "Real LLRs" begin
+        include("realLLR_tests.jl");
+    end
 
-    # @testset "LBeta" begin
-    #     include("lbeta_tests.jl");
-    # end
+    @testset "LBeta" begin
+        include("lbeta_tests.jl");
+    end
 
-    # @testset "Random LLRs" begin
-    #     include("randomLLR_tests.jl");
-    # end
+    @testset "Random LLRs" begin
+        include("randomLLR_tests.jl");
+    end
 
-    # @testset "Posterior probabilities" begin
-    #     include("postprobs_tests.jl");
-    # end
+    @testset "Posterior probabilities" begin
+        include("postprobs_tests.jl");
+    end
 
     @testset "Utils" begin
         include("utils_tests.jl");
